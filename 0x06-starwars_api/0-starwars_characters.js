53a1828452e498c9ac3c4cc80de2a31561f6593c
Mark Manani <mikonani413@gmail.com>
2024-11-12 18:09:41 +0300

    Task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const process = require('process');
+
+// Get the Movie ID from the command line arguments
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error("Usage: ./0-starwars_characters.js <Movie ID>");
+  process.exit(1);
+}
+
+// URL for the Star Wars API films endpoint
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+// Fetch the movie details
+axios.get(url)
+  .then(response => {
+    const characters = response.data.characters;
+    const characterPromises = characters.map(characterUrl => axios.get(characterUrl));
+
+    // Fetch all character names in order
+    return Promise.all(characterPromises);
+  })
+  .then(responses => {
+    responses.forEach(response => {
+      console.log(response.data.name);
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error.message);
+  });
